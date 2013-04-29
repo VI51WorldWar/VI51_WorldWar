e9151c95e123b5fcff828d8fad1d948dc662dab7
Théophile MADET <theo.madet@gmail.com>
2013-04-29 19:48:35 +0200

    Changed LandType to contain its own textures. Modified Panel class accordingly.

--- src/fr/utbm/vi51/environment/LandType.java
+++ src/fr/utbm/vi51/environment/LandType.java
@@ -1,5 +1,11 @@
 package fr.utbm.vi51.environment;
 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public enum LandType {
     // Enumeration of different types of land available in the game
     GRASS(1, true, "img/grass.png"), WATER(2, true, "img/water.png"), WALL(0, false, "img/wall.png");
@@ -9,13 +15,21 @@ public enum LandType {
     // Is this type of floor crossable ?
     private final boolean isCrossable;
     // Path of the land's texture
-    private final String texturePath;
+    private final Image texture;
 
     // Enumerator constructor
     LandType(int cost, boolean isCrossable, String texturePath) {
         this.cost = cost;
         this.isCrossable = isCrossable;
-        this.texturePath = texturePath;
+        Image tmp;
+        try {
+			tmp = ImageIO.read(new File(texturePath));
+		} catch (IOException e) {
+			tmp = null;
+			e.printStackTrace();
+		}
+        this.texture = tmp;
+		
     }
 
     public int getCost() {
@@ -26,8 +40,8 @@ public enum LandType {
         return isCrossable;
     }
 
-    public String getTexturePath() {
-        return texturePath;
+    public Image getTexture() {
+        return texture;
     }
 
 }
--- src/fr/utbm/vi51/gui/Panel.java
+++ src/fr/utbm/vi51/gui/Panel.java
@@ -15,33 +15,14 @@ import fr.utbm.vi51.environment.WorldObject;
 
 public class Panel extends JPanel {
     public void paintComponent(Graphics g) {
-    	// Loading of the differents textures
-    	Image grassTexture = null;
-    	Image waterTexture = null;
-    	Image wallTexture = null;
-    	try {
-	    	grassTexture = ImageIO.read(new File(LandType.GRASS.getTexturePath()));
-	    	waterTexture = ImageIO.read(new File(LandType.WATER.getTexturePath()));
-	    	wallTexture = ImageIO.read(new File(LandType.WALL.getTexturePath()));
-    	} catch (IOException e) {
-            e.printStackTrace();
-        }
     	
         try {
             Environment env = Environment.getInstance();
             Square[][][] map = env.getMap();
             for (int i = 0; i < 800 / 50; i++) {
                 for (int j = 0; j < 600 / 44; j++) {
-                    // Select the appropriate texture
-                	Image landTexture = null;
-                	// Depending of the square landtype
-                	switch(map[i][j][0].getLandType()) {
-	                	case GRASS : 	landTexture = grassTexture; 	break;
-	                	case WATER : 	landTexture = waterTexture;		break;
-	                	case WALL :		landTexture = wallTexture;		break;
-                	}
                 	// Draw land
-                	g.drawImage(landTexture, 50 * i, 44 * j, 50, 50, this);
+                	g.drawImage(map[i][j][0].getLandType().getTexture(), 50 * i, 44 * j, 50, 50, this);
                 	// Draw World's objects on the square
                     for (WorldObject obj : map[i][j][0].getObjects()) {
                         Image img1 = ImageIO.read(new File(obj.getTexture()));
